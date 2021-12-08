5e5ee143ae34f2c65f7d4cc500120433cdfb9722
MercersKitchen <Mark.Mercer@epsb.ca>
2021-12-08 10:19:29 -0700

    Update

---
+++ Pong OPP Lessons/03 Class Level Metaphors/Teacing with Size/Black Hole Metaphor/Unsecure/Beginning Pure Java Class/01 Boiler Plate/BlackHoleMetaphor/Star.java
@@ -0,0 +1,14 @@
+class Star {
+  //Global Variables, encapsulated
+  //
+  // Variable Population
+  Star() {
+  }//End Constructor
+  //
+  void draw() {}//End draw()
+  //
+  void step() {}//End step()
+  //
+  //Getters and Setters
+  //
+}//End Star
---
+++ Pong OPP Lessons/03 Class Level Metaphors/Teacing with Size/Black Hole Metaphor/Unsecure/Beginning Pure Java Class/02 Basic Driver and Pure Java Class/BlackHoleMetaphor/BlackHoleMetaphor.pde
@@ -0,0 +1,9 @@
+//Global Variables et al
+
+void setup() {}//End setup()
+
+void draw() {}//End draw()
+
+void mousePressed() {}//End mousePressed()
+
+void keyPressed() {}//End keyPressed()
