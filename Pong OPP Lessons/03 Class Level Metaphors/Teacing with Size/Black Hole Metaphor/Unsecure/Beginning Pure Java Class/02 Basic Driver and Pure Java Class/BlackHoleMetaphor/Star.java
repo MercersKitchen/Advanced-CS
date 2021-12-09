b103c806230e28aaba7fd97a9fcbd5f57894c590
MercersKitchen <Mark.Mercer@epsb.ca>
2021-12-09 10:09:37 -0700

    Update

--- Pong OPP Lessons/03 Class Level Metaphors/Teacing with Size/Black Hole Metaphor/Unsecure/Beginning Pure Java Class/02 Basic Driver and Pure Java Class/BlackHoleMetaphor/Star.java
+++ Pong OPP Lessons/03 Class Level Metaphors/Teacing with Size/Black Hole Metaphor/Unsecure/Beginning Pure Java Class/02 Basic Driver and Pure Java Class/BlackHoleMetaphor/Star.java
@@ -1,13 +1,23 @@
+/* Color is not a pure Java option, only Processing-Java
+ requires additional libraries Processing loads always
+ Note: Pure Java is best for text-based and arithmetic
+ */
+//
 class Star {
   //Global Variables, encapsulated
+  float x, y, diameter;
   //
   // Variable Population
-  Star() {
+  Star(float xParameter, float yParameter, float diameterParameter) {
+    this.x = xParameter;
+    this.y = yParameter;
+    this.diameter = diameterParameter;
   }//End Constructor
   //
   void draw() {}//End draw()
   //
-  void step() {}//End step()
+  void step() {
+  }//End step()
   //
   //Getters and Setters
   //
